98164cdbc7ae54c66350671ff38fe4cc9ae92d8e
e-goat <gorgonaly@gmail.com>
2025-06-02 14:16:19 +0300

    Fix punctuation and escape character in Info component

--- app/ui/info.tsx
+++ app/ui/info.tsx
@@ -2,7 +2,7 @@ export default function Info() {
   return (
     <section className="max-w-2xl mx-auto py-10 px-4">
         <p>
-          Hi, I'm Martin Duchev. You can find more about my projects on my{' '}
+          Hi, I&apos;m Martin Duchev. You can find more about my projects on my {" "}
           <a
             href="https://github.com/e-goat"
             className="text-blue-600 underline"
@@ -10,8 +10,7 @@ export default function Info() {
             rel="noopener noreferrer"
           >
             GitHub page
-          </a>
-          .
+          </a>.
         </p>
     </section>
   )
